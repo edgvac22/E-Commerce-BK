5e96281ac1b477e82478646a15b5fddb9ab6106b
edgvac2298 <edgardovac2298@gmail.com>
2021-12-09 16:52:03 -0500

    Login and Register

---
+++ registro.php
@@ -0,0 +1,44 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Registro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="css/signin.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+    <meta name="theme-color" content="#7952b3">
+  </head>
+
+<body class="text-center">
+    
+<main class="form-signin">
+  <form>
+    <img class="mb-4" src="img/logo.png" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Registro</h1>
+
+
+    <div class="form-floating">
+      <input type="text" class="form-control" id="floatingInput" placeholder="Nombre + Apellido">
+      <label for="floatingInput">Nombre completo</label>
+    </div>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="nombre@ejemplo.com">
+      <label for="floatingInput">Correo electrónico</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña">
+      <label for="floatingPassword">Contraseña</label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Crear cuenta</button>
+    <p class="para">¿Ya tienes cuenta? <a href="login.php" class="link-primary">Inicia sesión</a></p>
+    <p class="mt-5 mb-3 text-muted">&copy; Edgardo - 2021</p>
+  </form>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
