c3f572469b3ee3dea3d936cd5a75847effa4b956
edgvac2298 <edgardovac2298@gmail.com>
2021-12-12 08:39:17 -0500

    Footer ready

--- inicio.php
+++ inicio.php
@@ -45,6 +45,8 @@ if(isset($_REQUEST['correo']) && isset($_REQUEST['contra'])) {
 if (isset($_SESSION["iniciar_sesion"])) {
     ?>
 
+<!-- Header -->
+
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       <a href="inicio.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
@@ -73,6 +75,8 @@ if (isset($_SESSION["iniciar_sesion"])) {
       </div>
     </header>
   </div>
+
+    <!-- Carousel -->
 
   <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
@@ -125,11 +129,42 @@ if (isset($_SESSION["iniciar_sesion"])) {
     </button>
   </div>
 
-  <footer class="footer mt-auto py-3 bg-light">
-  <div class="container">
-    <span class="text-muted">&copy; Edgardo - 2021</span>
-  </div>
-</footer>
+    <!-- Servicios -->
+
+    <div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+        <h2>Heading</h2>
+        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+      </div>
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+        <h2>Heading</h2>
+        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+      </div>
+      <div class="col-lg-4">
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+        <h2>Heading</h2>
+        <p>And lastly this, the third column of representative placeholder content.</p>
+        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <hr class="featurette-divider">
+    <footer class="container">
+    <p class="float-end"><a href="inicio.php">Regresar al inicio</a></p>
+    <p>&copy; Edgardo Vaca - 2021 &middot; <a href="#">Políticas</a> &middot; <a href="#">Condiciones</a></p>
+  </footer>
+</main>
 
 <?php
 }
