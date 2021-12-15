277836348763fa231f23d46c27fd76a824ec939a
edgvac2298 <edgardovac2298@gmail.com>
2021-12-14 23:49:33 -0500

    Cart fixed

--- cart.php
+++ cart.php
@@ -80,14 +80,27 @@ if(!empty($_GET["action"])) {
                   }
                 }
                 break;
+                case "remove":
+                  if(!empty($_SESSION["cart_item"])) {
+                    foreach($_SESSION["cart_item"] as $k => $v) {
+                        if($_GET["code"] == $k)
+                          unset($_SESSION["cart_item"][$k]);				
+                        if(empty($_SESSION["cart_item"]))
+                          unset($_SESSION["cart_item"]);
+                    }
+                  }
+                break;
+                case "empty":
+                  unset($_SESSION["cart_item"]);
+                break;	
+              }     
         }
-    }
     ?>
     
     <div id="shopping-cart">
-<div class="txt-heading">Shopping Cart</div>
+<div class="txt-heading">Carrito de compras</div>
 
-<a id="btnEmpty" href="index.php?action=empty">Empty Cart</a>
+<a id="btnEmpty" href="cart.php?action=empty">Vaciar carrito</a>
 <?php
 if(isset($_SESSION["cart_item"])){
     $total_quantity = 0;
@@ -96,12 +109,12 @@ if(isset($_SESSION["cart_item"])){
 <table class="tbl-cart" cellpadding="10" cellspacing="1">
 <tbody>
 <tr>
-<th style="text-align:left;">Name</th>
-<th style="text-align:left;">Code</th>
-<th style="text-align:right;" width="5%">Quantity</th>
-<th style="text-align:right;" width="10%">Unit Price</th>
-<th style="text-align:right;" width="10%">Price</th>
-<th style="text-align:center;" width="5%">Remove</th>
+<th style="text-align:left;">Nombre</th>
+<th style="text-align:left;">Código</th>
+<th style="text-align:right;" width="5%">Cantidad</th>
+<th style="text-align:right;" width="10%">Precio unitario</th>
+<th style="text-align:right;" width="10%">Precio</th>
+<th style="text-align:center;" width="5%">Remover</th>
 </tr>	
 <?php		
     foreach ($_SESSION["cart_item"] as $item){
@@ -113,7 +126,7 @@ if(isset($_SESSION["cart_item"])){
 				<td style="text-align:right;"><?php echo $item["cantidad"]; ?></td>
 				<td  style="text-align:right;"><?php echo "$ ".$item["precio"]; ?></td>
 				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
-				<td style="text-align:center;"><a href="cart.php?action=remover&codigo=<?php echo $item["codigo"]; ?>" class="btnRemoveAction"><img src="img/icon-delete.png" alt="Remove Item" /></a></td>
+				<td style="text-align:center;"><a href="cart.php?action=remove&code=<?php echo $item["codigo"]; ?>" class="btnRemoveAction"><img src="img/icon-delete.png" alt="Remove Item" /></a></td>
 				</tr>
 				<?php
 				$total_quantity += $item["cantidad"];
