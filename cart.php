fabbcfd608b0fc83c2598ff83b09df69771f139f
edgvac2298 <edgardovac2298@gmail.com>
2021-12-14 23:56:03 -0500

    Cart ready css + functionality

--- cart.php
+++ cart.php
@@ -15,6 +15,7 @@ if (isset($_SESSION["iniciar_sesion"])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- CSS Interno + icon + theme color -->
     <link href="css/inicio.css" rel="stylesheet">
+    <link href="css/cart.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="img/logo.png">
     <meta name="theme-color" content="#7952b3">
 </head>
